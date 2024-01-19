046cab6c9cf02daaafc3467522b44d981d2f8086
Oumaïma <120368654+oumaimabenaboud@users.noreply.github.com>
2024-01-19 14:30:32 +0100

    ljadid

--- gestion_commandes_webapp/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {AdminDashboardComponent} from "./admin-dashboard/admin-dashboard.component";
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch: 'full' },
-  {
-    path : "admin",component: AdminDashboardComponent
-  },
-];
