cdc399f93c63886dac625d28ac8335e0cbef1319
YassineOurara <yassineourara@gmail.com>
2024-01-19 17:19:42 +0100

    login/register

--- gestion-commande-web-app/src/app/admin-dashboard/admin-dashboard.component.ts
+++ gestion-commande-web-app/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -5,7 +5,7 @@ import {CommandesService} from "../services/commandes.service";
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']  
+  styleUrls: ['./admin-dashboard.component.css']  // Fix the typo here
 })
 
 export class AdminDashboardComponent implements OnInit {
